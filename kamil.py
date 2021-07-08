a9f7ec543119cbd5c9d1834fab42027f0d4dd797
kbala42 <kbala42@aim.com>
2021-07-08 05:12:56 +0000

    initialize commit

---
+++ kamil.py
@@ -0,0 +1,10 @@
+class Kisi:
+	def __init__(self, ad, soyAd):
+		self.ad=ad
+		self.soyAd=soyAd
+
+	def Karsilama(self):
+		print("Merhaba ",self.ad, " ", self.soyAd)
+
+kisiBir=Kisi("Kaan","Çelik")
+kisiBir.Karsilama()
